cd01d087a6e9dd378c1bf4dff491e9f5b8e9f860
Proxx07 <zakir.pulatov97@gmail.com>
2025-02-21 19:49:18 +0500

    added composable returning type

--- composables/useRegions/index.ts
+++ composables/useRegions/index.ts
@@ -1,7 +1,19 @@
+import type { CookieRef } from '#app';
+import type { ComputedRef, Ref } from 'vue';
 import type { IRegion } from './types';
 import { centerCoordsDefault } from './models';
 
-export const useRegions = () => {
+interface RegionsComposable {
+  regions: ComputedRef<IRegion>[]
+  loading: ComputedRef<boolean>
+  activeRegion: ComputedRef<IRegion | undefined>
+  currentRegionId: CookieRef<number>
+  activeRegionCenterCoords: Ref<[number, number]>
+  setRegionId: (value: number) => void
+  getRegions: Promise<void>
+}
+
+export const useRegions = (): RegionsComposable => {
   const currentRegionId = useCookie<number>('region', { default: () => 0, maxAge: COOKIES_MAX_AGE });
 
   const { data, status, refresh } = useApi<IRegion[]>('/api/regions/List');
